dfa66801b5be6716545c1883784e1469f66b1512
Marcos Santiago Rivera (CTS-SI) <msantiago@ocpr.gov.pr>
2023-05-08 16:08:53 -0400

    Added an errorCatcher

--- routes/user/user.js
+++ routes/user/user.js
@@ -3,47 +3,57 @@ const express = require("express");
 const router = express.Router();
 const Admin = require("../../models/admin");
 const User = require("../../models/user");
+const catchAsync = require("../../utils/catchAsync");
 
-router.get("/profile", async (req, res) => {
-  //find admin by id
+router.get(
+  "/profile",
+  catchAsync(async (req, res) => {
+    //find admin by id
 
-  const admin = await Admin.findOne({ userId: req.user._id }).populate(
-    "groups"
-  );
+    const admin = await Admin.findOne({ userId: req.user._id }).populate(
+      "groups"
+    );
 
-  res.render("main/userPage/userPage", { admin });
-});
+    res.render("main/userPage/userPage", { admin });
+  })
+);
 
-router.get("/profile/edit", async (req, res) => {
-  res.render("main/userPage/editUserPage");
-});
+router.get(
+  "/profile/edit",
+  catchAsync(async (req, res) => {
+    res.render("main/userPage/editUserPage");
+  })
+);
 
 //make a patch request to update the user's profile
-router.patch("/profile/edit/:id", async (req, res) => {
-  const { id } = req.params;
+router.patch(
+  "/profile/edit/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
 
-  const { username, name, surname, city, phone, email } = req.body;
-  console.log(req.body);
-  if (username === req.user.username) {
-    const user = await User.findByIdAndUpdate(id, {
-      name,
-      surname,
-      city,
-      phone,
-      email,
-    });
-    res.redirect("/profile");
-  } else {
-    const user = await User.findByIdAndUpdate(id, {
-      username,
-      name,
-      surname,
-      city,
-      phone,
-      email,
-    });
-    res.redirect("/");
-  }
-});
+    const { username, name, surname, city, phone, email } = req.body;
+    console.log(req.body);
+    if (username === req.user.username) {
+      const user = await User.findByIdAndUpdate(id, {
+        name,
+        surname,
+        city,
+        phone,
+        email,
+      });
+      res.redirect("/profile");
+    } else {
+      const user = await User.findByIdAndUpdate(id, {
+        username,
+        name,
+        surname,
+        city,
+        phone,
+        email,
+      });
+      res.redirect("/");
+    }
+  })
+);
 
 module.exports = router;
