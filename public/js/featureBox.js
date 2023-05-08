197bb9c4e62bf716072fd5e1fd88d53eaf658b0a
Marcos Santiago Rivera (CTS-SI) <msantiago@ocpr.gov.pr>
2023-05-08 16:03:27 -0400

    added email,and fixed titles

--- public/js/featureBox.js
+++ public/js/featureBox.js
@@ -5,44 +5,17 @@ const featuredItem = document.querySelectorAll("#featureditem");
 const Itemimage = document.querySelectorAll("#image");
 const Maintitle = document.querySelectorAll("#tituloDeFeatured");
 
-const rect = featureBox.getBoundingClientRect();
+//make am event listener for when the window loads
+
+let rect = featureBox.getBoundingClientRect();
 for (i = 0; i < Maintitle.length; i++) {
   Maintitle[i].style.marginLeft = rect.left + "px";
 }
 
-for (let i = 0; i < featuredItem.length; i++) {
-  featuredItem[i].style.width = `${featureBox.clientWidth * 0.121}px`;
-  featuredItem[i].style.height = `${featureBox.clientWidth * 0.1439}px`;
-  featuredItem[i].style.margin = `0 ${featuredItem[i].clientWidth * 0.016}px`;
-  featuredItem[i].style.fontSize = `${featuredItem[i].clientWidth * 0.0062}px`;
-  Itemimage[i].style.width = `${featuredItem[i].clientWidth * 0.86}px`;
-  Itemimage[i].style.height = `${Itemimage[i].clientWidth * 0.6}px`;
-  Itemimage[i].style.marginTop = `${featuredItem[i].clientWidth * 0.052}px`;
-}
-
-let scrollbarWidth = scrollbar.offsetWidth - scrollbar.clientWidth;
-scrollbar.style.height = `${featuredItem[0].clientHeight + 17}px`;
-featureBox.style.height = `${scrollbar.clientWidth * 0.2917}px`;
-featureBox.style.fontSize = `${featureBox.clientWidth * 0.0062}px`;
-
 window.addEventListener("resize", () => {
-  const rect = featureBox.getBoundingClientRect();
+  console.log("HOAL");
+  let rect = featureBox.getBoundingClientRect();
   for (i = 0; i < Maintitle.length; i++) {
-    Maintitle[i].style.marginLeft = rect.left + "px";
+    Maintitle[0].style.marginLeft = rect.left + "px";
   }
-  featureBox.style.width = `${scrollbar.clientWidth * 2}px`;
-  featureBox.style.height = `${scrollbar.clientWidth * 0.2917}px`;
-
-  for (let i = 0; i < featuredItem.length; i++) {
-    featuredItem[i].style.width = `${featureBox.clientWidth * 0.121}px`;
-    featuredItem[i].style.height = `${featureBox.clientWidth * 0.1439}px`;
-    featuredItem[i].style.margin = `0 ${featuredItem[i].clientWidth * 0.016}px`;
-    featuredItem[i].style.fontSize = `${
-      featuredItem[i].clientWidth * 0.0062
-    }px`;
-    Itemimage[i].style.width = `${featuredItem[i].clientWidth * 0.86}px`;
-    Itemimage[i].style.height = `${Itemimage[i].clientWidth * 0.6}px`;
-    Itemimage[i].style.marginTop = `${featuredItem[i].clientWidth * 0.052}px`;
-  }
-  scrollbar.style.height = `${featuredItem[0].clientHeight + 17}px`;
 });
--- public/js/fitImage.js
+++ public/js/fitImage.js
@@ -1,8 +1,11 @@
 const images = document.querySelectorAll("#InnerCarousel");
 
-for (let i = 0; i < images.length; i++) {
-  images[i].style.height = images[i].clientWidth / 2.5 + "px";
-}
+//do an event listener for when the window loads
+window.addEventListener("load", () => {
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.height = images[i].clientWidth / 2.5 + "px";
+  }
+});
 
 window.addEventListener("resize", () => {
   for (let i = 0; i < images.length; i++) {
