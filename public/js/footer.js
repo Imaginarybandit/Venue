78ae0b0a3cdbe623550a75b712faf1241851a9a8
Marcos Santiago Rivera (CTS-SI) <msantiago@ocpr.gov.pr>
2023-05-11 08:27:19 -0400

    Half fixed the footer

---
+++ public/js/footer.js
@@ -0,0 +1,32 @@
+const boilerBody = document.querySelector("#boilerBody");
+const footer = document.querySelector("#footer");
+const windowHeight = window.innerHeight;
+
+// get boilerbody vh
+let boilerBodyVh = boilerBody.clientHeight;
+
+if (boilerBodyVh > windowHeight) {
+  let boilerBodyBottom = boilerBody.getBoundingClientRect().bottom;
+
+  let footerBottom = footer.getBoundingClientRect().bottom;
+
+  footer.style.bottom = boilerBodyBottom - footerBottom;
+} else {
+  footer.style.bottom = 0;
+}
+
+window.addEventListener("resize", () => {
+  boilerBodyVh = boilerBody.clientHeight;
+
+  if (boilerBodyVh > windowHeight) {
+    let boilerBodyBottom = boilerBody.getBoundingClientRect().bottom;
+
+    let footerBottom = footer.getBoundingClientRect().bottom;
+    console.log(boilerBodyBottom - windowHeight);
+    //make a margin top for footer
+
+    footer.style = `-${
+      boilerBodyBottom - windowHeight + footer.clientHeight
+    }px`;
+  }
+});
